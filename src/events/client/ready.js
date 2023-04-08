ea3ff5e3b9ff19a3028a649d8b480d7fb3066b35
SiroKu1006 <liu25864@gmail.com>
2023-04-08 17:46:21 +0800

    uptime reboot notice

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,5 +3,11 @@ module.exports = {
     once : true,
     async execute(client){
         console.log(`${client.user.tag} is online.`)
+        user = process.env.MyDiscordId
+        try {
+            client.users.send(user, 'Uptime Reboot')
+        } catch (err) {
+            console.error(err)
+        }
     }
 }
